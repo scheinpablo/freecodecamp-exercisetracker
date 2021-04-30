81524851b07faf8856a5d29de3f87902e5785fa8
scheinpablo <scheinpablo@yahoo.com>
2021-04-30 01:35:44 -0300

    date bug fixed

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.post('/api/users/:_id/exercises', (req, res) => {
   let newExercise = {
     description: req.body.description,
     duration: req.body.duration,
-    date: (req.body.date) ? req.body.date: new Date().toISOString().substring(0,10),
+    date: (req.body.date) ? req.body.date: new Date().toDateString(),
   };
   User.findByIdAndUpdate(req.params._id, { $push: { log: newExercise }, $inc: { count: 1 } }, { new: true }, (err, data) => {
     if (err) return res.json({ error: 'invalid id' });
